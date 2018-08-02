cc19ed84e4028db02d761d0a31e1ca2b42be775e
Laimonas <lmatukas@gmail.com>
2018-08-02 14:06:43 +0300

    CacheTest edit

--- JFight/test/Test.java
+++ JFight/test/Test.java
@@ -1,46 +1,46 @@
-import main.Models.CONS.FighterStatus;
-import main.Services.IDataBase;
-import main.Services.Impl.DataBase;
-
-import static org.junit.Assert.assertTrue;
-
-public class Test {
-
-    @org.junit.Test
-    public void DbgetConnectionTest() throws Exception {
-        IDataBase dataBase = new DataBase();
-        assertTrue(dataBase.getConnection() != null);
-    }
-
-//    @org.junit.Test
-//    public void ReadDbTest() throws Exception {
+//import main.Models.CONS.FighterStatus;
+//import main.Services.IDataBase;
+//import main.Services.Impl.DataBase;
 //
-//        String query = "select Email from [User]";
-//        Crud crud = new Crud();
-//        assertTrue(crud.read(query)!=null);
-//    }
-
-    @org.junit.Test
-    public void ReadyStatus() {
-        assertTrue(FighterStatus.SUCCESS != null);
-        System.out.println(FighterStatus.SUCCESS);
-        System.out.println(FighterStatus.FAILURE);
-    }
-
-//    @org.junit.Test
-//    public void getReadyToFightUsersExceptPrimaryUser() {
-//        FighterService fighterService = new FighterService();
-//        assertTrue(fighterService.getReadyToFightUsersExceptPrimaryUser()!=null);
-//        DBqueryDTO dBqueryDTO = new Crud().read(query);
-//        System.out.println( dBqueryDTO.list);
-//        System.out.println( dBqueryDTO.list.isEmpty());
-//    }
-
-//    @org.junit.Test
-//    public void find() {
-//        HigherService hs = new HigherService();
+//import static org.junit.Assert.assertTrue;
 //
-//     DBqueryDTO dto = new
+//public class Test {
+//
+//    @org.junit.Test
+//    public void DbgetConnectionTest() throws Exception {
+//        IDataBase dataBase = new DataBase();
+//        assertTrue(dataBase.getConnection() != null);
 //    }
-}
-
+//
+////    @org.junit.Test
+////    public void ReadDbTest() throws Exception {
+////
+////        String query = "select Email from [User]";
+////        Crud crud = new Crud();
+////        assertTrue(crud.read(query)!=null);
+////    }
+//
+//    @org.junit.Test
+//    public void ReadyStatus() {
+//        assertTrue(FighterStatus.SUCCESS != null);
+//        System.out.println(FighterStatus.SUCCESS);
+//        System.out.println(FighterStatus.FAILURE);
+//    }
+//
+////    @org.junit.Test
+////    public void getReadyToFightUsersExceptPrimaryUser() {
+////        FighterService fighterService = new FighterService();
+////        assertTrue(fighterService.getReadyToFightUsersExceptPrimaryUser()!=null);
+////        DBqueryDTO dBqueryDTO = new Crud().read(query);
+////        System.out.println( dBqueryDTO.list);
+////        System.out.println( dBqueryDTO.list.isEmpty());
+////    }
+//
+////    @org.junit.Test
+////    public void find() {
+////        HigherService hs = new HigherService();
+////
+////     DBqueryDTO dto = new
+////    }
+//}
+//
--- JFight/test/main/Services/Impl/CacheTest.java
+++ JFight/test/main/Services/Impl/CacheTest.java
@@ -11,17 +11,22 @@ public class CacheTest {
 
     @Test
     public void getInsertedObject() {
-
         Integer integer = new Integer(12);
         cache.put(key, integer);
         Object object = cache.get(key);
-        assertTrue(!object.equals(null));
+        System.out.println("OBJ- " + object.toString());
+        assertTrue(object != null);
+        System.out.println("cia");
+//        assertTrue((object instanceof String));
+//        System.out.println("-------");
     }
+
 
     @Test
     public void removeInsertedObject() {
         cache.remove(key);
         Object object = cache.get(key);
+        System.out.println("OBJ- " + object.toString());
         assertTrue(object.equals(null));
     }
 }
