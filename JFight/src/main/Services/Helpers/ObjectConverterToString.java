f8d481eaee804f8bf081ab273cb71e9f15d52753
Motiejus <motiejus.simonis@gmail.com>
2018-08-14 10:38:53 +0300

    [ObjectConverterToString] (MS) Refactor -> convertObject() method catch section was changed from trowing runtime exeption to returning empty hashMap<>().

--- JFight/src/main/Services/Helpers/ObjectConverterToString.java
+++ JFight/src/main/Services/Helpers/ObjectConverterToString.java
@@ -27,8 +27,8 @@ public final class ObjectConverterToString {
             return map;
         }
         catch (Exception e) {
-        	//Review. We go error, so we throw cooler error? lol. How about we log it?
-            throw new RuntimeException(e.getMessage());
+            e.printStackTrace();
+            return new HashMap<>();
         }
     }
 }
